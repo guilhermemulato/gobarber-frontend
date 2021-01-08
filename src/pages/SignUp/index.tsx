c0d12cf46fad1bd0b65837c824522e08439afef1
Guilherme Mulato <guilherme_mulato@hotmail.com>
2021-01-08 00:44:51 -0300

    Melhora da usabilidade dos inputs

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,36 +1,42 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
-import { FiLogIn, FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 import { Container, Content, Background } from './styles';
 
-const SingIn: React.FC = () => (
-    <Container>
-        <Background />
-        <Content>
-            <img src={logo} alt="goBarber" />
-            <form>
-                <h1>Faça seu cadastro</h1>
+const SingIn: React.FC = () => {
+    function handleSubmit(data: any): void {
+        console.log(data);
+    }
+    return (
+        <Container>
+            <Background />
+            <Content>
+                <img src={logo} alt="goBarber" />
+                <Form onSubmit={handleSubmit}>
+                    <h1>Faça seu cadastro</h1>
 
-                <Input name="name" icon={FiUser} placeholder="Nome" />
-                <Input name="email" icon={FiMail} placeholder="Email" />
-                <Input
-                    name="password"
-                    icon={FiLock}
-                    type="password"
-                    placeholder="Senha"
-                />
-                <Button type="submit">Cadastrar</Button>
-            </form>
-            <a href="login">
-                <FiArrowLeft />
-                Voltar para logon
-            </a>
-        </Content>
-    </Container>
-);
+                    <Input name="name" icon={FiUser} placeholder="Nome" />
+                    <Input name="email" icon={FiMail} placeholder="Email" />
+                    <Input
+                        name="password"
+                        icon={FiLock}
+                        type="password"
+                        placeholder="Senha"
+                    />
+                    <Button type="submit">Cadastrar</Button>
+                </Form>
+                <a href="login">
+                    <FiArrowLeft />
+                    Voltar para logon
+                </a>
+            </Content>
+        </Container>
+    );
+};
 
 export default SingIn;
