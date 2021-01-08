fa5265acdeedb0f278e27db813af7f0f347ef465
Guilherme Mulato <guilherme_mulato@hotmail.com>
2021-01-07 23:31:37 -0300

    Desenvolvida pagina de login

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Content, Background } from './styles';
+
+const SingIn: React.FC = () => (
+    <Container>
+        <Content>
+            <img src={logo} alt="goBarber" />
+            <form>
+                <h1>Faça seu logon</h1>
+
+                <Input name="email" icon={FiMail} placeholder="Email" />
+                <Input
+                    name="password"
+                    icon={FiLock}
+                    type="password"
+                    placeholder="Senha"
+                />
+                <Button type="submit">Entrar</Button>
+                <a href="forgot">Esqueci minha senha</a>
+            </form>
+            <a href="login">
+                <FiLogIn />
+                Criar Conta
+            </a>
+        </Content>
+        <Background />
+    </Container>
+);
+
+export default SingIn;
